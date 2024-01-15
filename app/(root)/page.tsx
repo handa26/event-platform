6f33fa12c130342a4dc726790d64524d4e4a2e86
Ananda Muhammad <anandamuhammadmtq@gmail.com>
2024-01-15 10:48:54 +0800

    implement stripe payment

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,18 @@ import Link from "next/link";
 import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
+import Collection from "@/components/shared/Collection";
 
-export default function Home() {
+import { getAllEvents } from "@/lib/actions/event.actions";
+
+export default async function Home() {
+  const events = await getAllEvents({
+    query: "",
+    category: "",
+    page: 1,
+    limit: 6,
+  });
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -34,6 +44,16 @@ export default function Home() {
           <p>Search</p>
           <p>CategoryFilter</p>
         </div>
+
+        <Collection 
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          totalPages={2}
+        />
       </section>
     </>
   )
---
+++ components/shared/CheckoutButton.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { useUser, SignedOut, SignedIn } from "@clerk/nextjs";
+import Link from "next/link";
+
+import { Button } from "../ui/button";
+import Checkout from "./Checkout";
+
+import { IEvent } from "@/lib/database/models/event.model";
+
+const CheckoutButton = ({ event }: { event: IEvent }) => {
+  const { user } = useUser();
+  const userId = user?.publicMetadata.userId as string;
+
+  const hasEventFinished = new Date(event.endDateTime) < new Date();
+
+  return (
+    <div className="flex items-center gap-3">
+      {/* Cannot buy past event */}
+      {hasEventFinished ? (
+        <p className="p-2 text-red-400">
+          Sorry, tickets are no longer available.
+        </p>
+      ) : (
+        <>
+          <SignedOut>
+            <Button asChild size="lg" className="button rounded-full">
+              <Link href="/sign-in">Get Tickets</Link>
+            </Button>
+          </SignedOut>
+
+          <SignedIn>
+            <Checkout event={event} userId={userId} />
+          </SignedIn>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default CheckoutButton;
